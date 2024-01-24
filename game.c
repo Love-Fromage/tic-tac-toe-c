a2300b375b4738634a1821c7e24bedfaf91b495a
Love-Fromage <mathecool22@gmail.com>
2024-01-24 17:07:01 -0500

    je peux mieux formatter ma string

--- game.c
+++ game.c
@@ -3,6 +3,14 @@
 #include <unistd.h>
 
 
+const char* getInstructions(){
+    // Using a character array
+    
+    return "You always start as the X, therefore they are the first\n\
+to play. Each cell is reprensented by a number. To place your X simply\n\
+put input the number of the cell. Then it is the computer's turn.\n";
+}
+
 void demande(){
    printf("1. Play\n");
    printf("2. How to play\n");
@@ -21,7 +29,16 @@ void demande(){
         printf("You chose to play!\n");
         break;
     case 2:
-        printf("You chose the instructions menu!\n");
+        printf("You chose the instructions menu!\n\n");
+        const char* instructions = getInstructions();
+        // Here i print the instructions
+        printf("%s\n", instructions);
+        printf("1. Back\n");
+        int choice2;
+        scanf("%d", &choice2);
+        if(choice2 == 1){
+            demande();
+        }
         break;
     case 3:
         printf("You chose to quit the game!\n");
